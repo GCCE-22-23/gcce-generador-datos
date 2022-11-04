b33ef9faeba03fdd51799b8fc526c4fac35adaed
Claudio Yanes <me@claudio4.com>
2022-11-04 19:01:06 +0000

    Add randomItemFromArray to utils

--- src/generators/utils.ts
+++ src/generators/utils.ts
@@ -4,3 +4,7 @@ export function getItemByHash<T>(valueToHash: string, arr: T[]): T {
   const hash = crc32.str(valueToHash)
   return arr[hash % arr.length]
 }
+
+export function randomItemFromArray<T>(arr: T[]): T {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
