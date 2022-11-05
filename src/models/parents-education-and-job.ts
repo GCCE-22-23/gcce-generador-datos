f49dfcec0c755b630d653e8e07190f8dd31eedd7
Claudio Yanes <me@claudio4.com>
2022-11-05 01:12:33 +0000

    Add ParentsEducationAndJob model
    Fix #18

---
+++ src/models/parents-education-and-job.ts
@@ -0,0 +1,12 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from "../database/index.js"
+
+export const ParentsEducationAndJob = sequelize.define('FORMACION_Y_OCUPACION_DE_LOS_FAMILIARES', {
+  ID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
+  CURSO_ACA: DataTypes.STRING,
+  COD_VARIABLE: DataTypes.STRING,
+  VALOR: DataTypes.STRING
+})
