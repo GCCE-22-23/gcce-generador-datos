42c5ea894bf68a0f519738e3b08a15bc719cf837
Claudio Yanes <me@claudio4.com>
2022-11-05 01:02:51 +0000

    Add generator records

---
+++ src/data/subject.ts
@@ -0,0 +1,28 @@
+export const typologies = [
+  'T', // Formacion basica
+  'B', // Obligatoria
+  'O', // Optativa
+  'E', // Practicas externas
+  'P'  // TFG
+]
+
+export const call = [
+  'ENE',
+  'JUN',
+  'JUL',
+  'SEP'
+]
+
+export const mark = [
+  'NA',
+  'IN',
+  'NP',
+  'AC',
+  'RP',
+  'SS',
+  'A',
+  'AP',
+  'NT',
+  'SB',
+  'MH'
+]
