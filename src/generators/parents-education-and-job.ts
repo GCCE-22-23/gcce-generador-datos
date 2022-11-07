b8395de775b972bae521bceec99f6ac406ed8e14
Claudio Yanes <me@claudio4.com>
2022-11-07 16:33:48 +0000

    Finish generateParentsEducationAndJob

--- src/generators/parents-education-and-job.ts
+++ src/generators/parents-education-and-job.ts
@@ -1,12 +1,14 @@
+import crc32 from 'crc-32'
 import { academicYear } from "../data/academic-years.js";
 import { ParentsEducationAndJob } from "../models/parents-education-and-job.js";
 import { getItemByHash } from "./utils.js";
 
 export function generateParentsEducationAndJob(id: string) {
+  const variableCode = crc32.str(id) % 10
   return ParentsEducationAndJob.create({
     ID: id,
     CURSO_ACA: getItemByHash(id, academicYear),
-    COD_VARIABLE: 0, // IDK TODO
-    VALOR: "IDK" // IDK TODO
+    COD_VARIABLE: variableCode,
+    VALOR: variableCode.toString(),
   })
 }
