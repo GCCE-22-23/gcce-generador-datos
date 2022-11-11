0a9073c3db751a38d51fa095d82f0473ff65f9f9
alu0101225562 <alu0101225562@ull.edu.es>
2022-11-11 14:39:29 +0000

    hot fis

--- src/models/ebau_subjects.ts
+++ src/models/ebau_subjects.ts
@@ -3,13 +3,15 @@ import { sequelize } from "../database/index.js"
 
 export const EbauSubject = sequelize.define('EBAU_MATERIA', {
   ID: {
-     type: DataTypes.UUID
-,
+    type: DataTypes.UUID,
     primaryKey: true
   },
   CURSO_ACA: DataTypes.STRING,
   CONV: DataTypes.STRING,
-  COD_MATERIA: DataTypes.STRING,
+  COD_MATERIA: {
+    type: DataTypes.INTEGER,
+    primaryKey: true
+  },
   MATERIA: DataTypes.STRING,
   FASE:  DataTypes.STRING,
   NOTA: DataTypes.FLOAT
